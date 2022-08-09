36b67a6ee62f34aced156610fa4a6cb23d2e4c41
Kascha <65874443+Kaisa-Marysia@users.noreply.github.com>
2022-08-09 16:58:57 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+while true; 
+  do 
+    curl "wttr.in/Bielefeld?1npQ&lang=de"; 
+    sleep 10; 
+    clear; 
+    curl "wttr.in/moon@2022-08-09&land=de"; 
+    sleep 10; 
+    clear;
+done 
