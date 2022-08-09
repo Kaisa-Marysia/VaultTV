8a299fc3e3ecf587ab2166aac2ec82a49178b3aa
Kascha <65874443+Kaisa-Marysia@users.noreply.github.com>
2022-08-09 17:07:12 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,22 @@ while true;
   do 
     curl "wttr.in/Bielefeld?1npQ&lang=de"; 
     sleep 10; 
+    STARTPOS=1; 
+      while [ $STARTPOS -le 60 ]; 
+        do 
+          printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' '#'; 
+          STARTPOS=$(( $STARTPOS + 1 )); 
+          sleep 0.01; 
+        done
     clear; 
     curl "wttr.in/moon@2022-08-09&land=de"; 
     sleep 10; 
+    STARTPOS=1; 
+      while [ $STARTPOS -le 60 ]; 
+        do 
+          printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' '#'; 
+          STARTPOS=$(( $STARTPOS + 1 )); 
+          sleep 0.01; 
+        done
     clear;
 done 
